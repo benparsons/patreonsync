04902319a28aa817588341cc9b88f7dc8f35c375
Ben Parsons <ben@bpulse.co.uk>
2019-02-06 09:42:41 +0000

    avoid communities breaking

--- ps3/main.py
+++ ps3/main.py
@@ -11,7 +11,7 @@ from ps3.bot import Patroniser
 from ps3.subscribers import GuestListUtils
 
 from ps3.subscribers.patreon import PatreonGuestList
-from ps3.subscribers.patreon import PatreonGroupGuestList
+#from ps3.subscribers.patreon import PatreonGroupGuestList
 from ps3.subscribers import YamlGuestList
 
 from ps3.util.TokenRefresher import refresh_token
@@ -60,12 +60,13 @@ if __name__ == "__main__":
                                            staff])
 
     for room, guests in guest_list.iteritems():
-        community_guests = [guest[0] for guest in guests if guest[1]]
-        patroniser.enforce_group(room.replace('#', '+', 1),
-                                 community_guests,
-                                 actually_invite=args.do_group_invites,
-                                 actually_kick=args.do_group_kicks,
-                                 verbose=args.verbose)
+        if room != '#polynomial-supporters:matrix.org':
+            community_guests = [guest[0] for guest in guests if guest[1]]
+            patroniser.enforce_group(room.replace('#', '+', 1),
+                                    community_guests,
+                                    actually_invite=args.do_group_invites,
+                                    actually_kick=args.do_group_kicks,
+                                    verbose=args.verbose)
 
         room_guests = [guest[0] for guest in guests]
         patroniser.enforce(room, room_guests,
